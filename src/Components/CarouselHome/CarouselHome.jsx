e7a36f5d0ed777559480ebfd90a198171ce7e9e3
Manh Cuong <cuongmanh0910@gmail.com>
2023-02-27 15:56:02 +0700

    Carousel Home

---
+++ src/Components/CarouselHome/CarouselHome.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CarouselGlobal from "./CarouselGlobal";
+import './CarouselHome.css'
+function CarouselHome() {
+  return (
+    <>
+      <CarouselGlobal hasImage={false} ></CarouselGlobal>
+    </>
+  );
+}
+
+export default CarouselHome;
