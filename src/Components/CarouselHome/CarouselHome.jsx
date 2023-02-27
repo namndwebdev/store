e0b3697f98db5d00ef9d3bdab6f1f9ccf58a9a09
Manh Cuong <cuongmanh0910@gmail.com>
2023-02-27 16:35:07 +0700

    Cuong update

--- src/Components/CarouselHome/CarouselHome.jsx
+++ src/Components/CarouselHome/CarouselHome.jsx
@@ -2,9 +2,42 @@ import React from "react";
 import CarouselGlobal from "./CarouselGlobal";
 import './CarouselHome.css'
 function CarouselHome() {
+    const dataImage = [{
+        image: "./CarouselHome-IMG/slideshow_1.webp",
+      },
+      {
+        image: "./CarouselHome-IMG/slideshow_2.webp",
+      },
+      {
+        image: "./CarouselHome-IMG/slideshow_3.webp",
+      },
+      {
+        image: "./CarouselHome-IMG/slideshow_4.webp",
+      },
+      {
+        image: "./CarouselHome-IMG/slideshow_5.webp",
+      },
+      {
+        image: "./CarouselHome-IMG/slideshow_6.webp",
+      },
+      {
+        image: "./CarouselHome-IMG/slideshow_7.webp",
+      },
+      {
+        image: "./CarouselHome-IMG/slideshow_8.webp",
+      },
+      {
+        image: "./CarouselHome-IMG/slideshow_9.webp",
+      },
+      {
+        image: "./CarouselHome-IMG/slideshow_10.webp",
+      },
+      {
+        image: "./CarouselHome-IMG/slideshow_11.webp",
+      },]
   return (
     <>
-      <CarouselGlobal hasImage={false} ></CarouselGlobal>
+      <CarouselGlobal dataImage={dataImage} hasImage={false} data></CarouselGlobal>
     </>
   );
 }
