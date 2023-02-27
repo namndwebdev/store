ccf697db73c4a30d1fba6c8acae20b6db2b34b63
QuysNgoo <quysngoo06@gmail.com>
2023-02-27 16:12:20 +0700

    QuyNgoo

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Footer = () => {
+  return <>
+    <div className='footer'>
+        <div className='footer-promotion'>
+            <form action="">
+                <span className='footer-promotion__logo'></span>
+                <span>Mua hàng online với nhiều ưu đãi tại GEARVN</span>
+                <span></span>
+            </form>
+        </div>
+        <div className='footer-info'>
+            <div className='footer-info__left'></div>
+            <div className='footer-info__middle'></div>
+            <div className='footer-info__right'></div>
+        </div>
+    </div>
+  </>
+}
