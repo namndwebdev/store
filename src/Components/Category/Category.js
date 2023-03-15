fd7f0efd92becc996fa2c0e5958315be81d95b39
Manh Cuong <cuongmanh0910@gmail.com>
2023-03-15 16:02:04 +0700

    CuongUpdate

---
+++ src/Components/Category/Category.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import ProductList from "../ProductList/ProductList";
+import Header from "../Header/Header";
+import Baner from "../../Pages/Menu/Baner";
+import "../Category/Category.css";
+export default function Category() {
+  let [productList, setProductList] = useState([]);
+  let { catId } = useParams();
+
+  useEffect(() => {
+    fetch(
+      `https://backoffice.nodemy.vn/api/products?filters[idCategories][name][$eq]=${catId}&populate=*`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setProductList(res.data);
+      });
+  }, []);
+  return (
+    <div className="">
+      <Header></Header>
+      <div className="container">
+        <h2>Title</h2>
+        <div>breadcum</div>
+        <ProductList dataList={productList}></ProductList>
+      </div>
+    </div>
+  );
+}
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,87 +1,32 @@
-import ProductList from '../../Components/ProductList/ProductList';
-import { useState } from 'react';
-import MegaMenu from '../Menu/MegaMenu';
-import Header from '../../Components/Header/Header'
-import Breadcrumbs from '../../Components/NavBreadcrums/NavBreadcrums';
-import Baner from '../Menu/Baner'
-import SubHeader from '../../Components/Header/SubHeader';
-import { Footer } from 'antd/es/layout/layout';
+import ProductList from "../../Components/ProductList/ProductList";
+import { useState } from "react";
+import MegaMenu from "../Menu/MegaMenu";
+import Header from "../../Components/Header/Header";
+import Breadcrumbs from "../../Components/NavBreadcrums/NavBreadcrums";
+import Baner from "../Menu/Baner";
+import SubHeader from "../../Components/Header/SubHeader";
+import { Footer } from "antd/es/layout/layout";
+import { useEffect } from "react";
+import FooterMenu from "../Menu/FooterMenu";
 
 export default function Home() {
-  const [data, setData] = useState([
-    {
-        title: 'May tinh thu nhat 1',
-        beforeDiscount: '18.000.000d',
-        afterDiscount: '16.000.000d',
-        percentDiscount: '3%',
-        thumbnail: 'https://product.hstatic.net/1000026716/product/111_121e1c7911074629b99973a320b6c1b3_large.png',
-    },
-    {
-        title: 'May tinh thu nhat 2',
-        beforeDiscount: '18.000.000d',
-        afterDiscount: '16.000.000d',
-        percentDiscount: '3%',
-        thumbnail: 'https://product.hstatic.net/1000026716/product/111_121e1c7911074629b99973a320b6c1b3_large.png',
-    },
-    {
-        title: 'May tinh thu nhat 1',
-        beforeDiscount: '18.000.000d',
-        afterDiscount: '16.000.000d',
-        percentDiscount: '3%',
-        thumbnail: 'https://product.hstatic.net/1000026716/product/111_121e1c7911074629b99973a320b6c1b3_large.png',
-    },
-    {
-        title: 'May tinh thu nhat 1',
-        beforeDiscount: '18.000.000d',
-        afterDiscount: '16.000.000d',
-        percentDiscount: '3%',
-        thumbnail: 'https://product.hstatic.net/1000026716/product/111_121e1c7911074629b99973a320b6c1b3_large.png',
-    },
-    {
-        title: 'May tinh thu nhat 2',
-        beforeDiscount: '18.000.000d',
-        afterDiscount: '16.000.000d',
-        percentDiscount: '3%',
-        thumbnail: 'https://product.hstatic.net/1000026716/product/111_121e1c7911074629b99973a320b6c1b3_large.png',
-    },
-    {
-        title: 'May tinh thu nhat 1',
-        beforeDiscount: '18.000.000d',
-        afterDiscount: '16.000.000d',
-        percentDiscount: '3%',
-        thumbnail: 'https://product.hstatic.net/1000026716/product/111_121e1c7911074629b99973a320b6c1b3_large.png',
-    },
-    {
-        title: 'May tinh thu nhat 1',
-        beforeDiscount: '18.000.000d',
-        afterDiscount: '16.000.000d',
-        percentDiscount: '3%',
-        thumbnail: 'https://product.hstatic.net/1000026716/product/111_121e1c7911074629b99973a320b6c1b3_large.png',
-    },
-    {
-        title: 'May tinh thu nhat 2',
-        beforeDiscount: '18.000.000d',
-        afterDiscount: '16.000.000d',
-        percentDiscount: '3%',
-        thumbnail: 'https://product.hstatic.net/1000026716/product/111_121e1c7911074629b99973a320b6c1b3_large.png',
-    },
-    {
-        title: 'May tinh thu nhat 1',
-        beforeDiscount: '18.000.000d',
-        afterDiscount: '16.000.000d',
-        percentDiscount: '3%',
-        thumbnail: 'https://product.hstatic.net/1000026716/product/111_121e1c7911074629b99973a320b6c1b3_large.png',
-    },
-]);
+  let [data, setData] = useState([]);
 
-return (
+  useEffect(() => {
+    fetch(`https://backoffice.nodemy.vn/api/products?populate=*`)
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res.data);
+      });
+  }, []);
+  console.log(data);
+  return (
     <>
-        <Header></Header>
-        <Baner></Baner>
-        <MegaMenu />
-        <ProductList data={data} title="text- 1"></ProductList>
-        <ProductList data={data} title="text -2"></ProductList>
-        <Footer></Footer>
+      <Header></Header>
+      <Baner></Baner>
+      <ProductList dataList={data}></ProductList>
+      <ProductList dataList={data}></ProductList>
+
     </>
-);
+  );
 }
