cdb371206f47f0590a91319c79deea5cd278c8a5
Manh Cuong <cuongmanh0910@gmail.com>
2023-03-15 16:10:54 +0700

    CuongUpdate

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -2,15 +2,20 @@ import React, { useState } from "react";
 import "./ProductList.css";
 import Product from "../Product/Product";
 
-const ProductList = ({ dataList }) => {
+const ProductList = ({ dataList, home = false }) => {
   return (
     <div className="product-list-container">
-      <h2 className="product-list__title">PC GEARVN - MIỄN PHÍ GIAO HÀNG TOÀN QUỐC</h2>
-    <div className="row">
-      {dataList.map((data) => (
-        <Product data={data}></Product>
-      ))}
-    </div>
+      {home ? (
+        <h2 className="product-list__title">
+          PC GEARVN - MIỄN PHÍ GIAO HÀNG TOÀN QUỐC
+        </h2>
+      ) : null}
+
+      <div className="row">
+        {dataList.map((data) => (
+          <Product data={data}></Product>
+        ))}
+      </div>
     </div>
   );
 };
