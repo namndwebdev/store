0b0f4f6a18cf20bae4981e47e7380e8429a1afe7
QuysNgoo <quysngoo06@gmail.com>
2023-03-16 11:41:17 +0700

    Quys update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,110 +1,71 @@
-import React, { useEffect, useState } from "react";
-// import logoHeader from '../../static/svg/logo-header'
-import {
-  UilNotes,
-  UilUserCircle,
-  UilTicket,
-  UilShoppingCart,
-  UilSearch,
-  UilPhone,
-  UilYoutube,
-  UilUser,
-  UilArchway,
-} from "@iconscout/react-unicons";
-import "./header.css";
-import SubHeader from "./SubHeader";
+import React, { useEffect, useState } from 'react'
+import { UilNotes,UilUserCircle,UilTicket,UilShoppingCart,UilSearch,UilPhone,UilYoutube,UilUser,UilArchway  } from '@iconscout/react-unicons'
+import './header.css'
+import SubHeader from './SubHeader'
 
 const Header = () => {
-  const [dataHeader, setDataHeader] = useState([]);
 
-  useEffect(() => {
-    fetch(
-      "https://backoffice.nodemy.vn/api/menu-headers?populate[menuheader][populate][0]=link"
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setDataHeader(data.data);
-      });
-  }, []);
-  // console.log(dataHeader[0].attributes.menuheader.link)
+    const [dataHeader, setDataHeader] = useState([])
+    useEffect(() => {
+        fetch('https://backoffice.nodemy.vn/api/menu-headers?populate[menuheader][populate][0]=link')
+        .then((res) => res.json())
+        .then((data) => {
+            setDataHeader(data.data)
+        
+        })
+    }, [])
 
   return (
     //
     <>
-      <div className="header">
-        <div className=" container">
-          <div className="header-item">
-            <div className="left-header">
-              <img
-                src={"https://via.placeholder.com/170x52.05/green"}
-                alt="logo"
-              />
-            </div>
-            <div className="right-header">
-              <div className="right-header__line1">
-                <div className="searchbox-header">
-                  <input
-                    className="input-header"
-                    type="text"
-                    placeholder="Nhập mã sản phẩm hoặc tên sản phẩm"
-                  />
-                  <button className="button-header">
-                    <UilSearch
-                      className="button-header__icon"
-                      style={{
-                        width: "17px",
-                        height: "17px",
-                        fontWeight: "700",
-                      }}
-                    ></UilSearch>
-                  </button>
+        <div className='header'>
+            <div className='header-menu'>
+                <div className='left-header'>
+                    <img src="https://via.placeholder.com/170x52.05/green" alt="" />
                 </div>
-
-                <span>
-                  {dataHeader &&
-                    dataHeader.length > 0 &&
-                    dataHeader[0].attributes.menuheader.link.map((item) => {
-                      return (
-                        <span>
-                          <span style={{ marginLeft: "15px" }}>
-                            <UilNotes></UilNotes>
-                            <span
-                              style={{ fontWeight: "700", fontSize: "13px" }}
-                            >
-                              {item.label}
-                            </span>
-                          </span>
+                <div className='right-header'>
+                    <div className="right-header__line1">
+                        <div className='header-search'>
+                            <input className='header-search__input' type="text" placeholder='Nhập mã hoặc tên sản phẩm...'/>
+                            <button className='header-search__button'><UilSearch style={{width:'17px',height:'17px', fontWeight:'700'}}></UilSearch></button>
+                        </div>
+                        <span className='right-header__line1-menu' >
+                            {
+                                dataHeader && dataHeader.length > 0 && dataHeader[0].attributes.menuheader.link.map((item) => {
+                                    return <span>
+                                
+                                    <span style={{ marginLeft:'15px' }}>
+                                    <span className='top-header__icon' dangerouslySetInnerHTML={{__html:item.icon}}/>
+                                        <span style={{fontWeight:'700',fontSize:'15px'}}>{item.label}</span>
+                                    </span>
+                                </span>
+                                })
+                            }
                         </span>
-                      );
-                    })}
-                </span>
-              </div>
-              <div className="right-header__line2">
-                <span>
-                  {dataHeader &&
-                    dataHeader.length > 0 &&
-                    dataHeader[1].attributes.menuheader.link.map(
-                      (item, index) => {
-                        return (
-                          <>
-                            <span className={index !== 3 && "span-left"}>
-                              <UilPhone></UilPhone>
-                              <span>{item.label}</span>
-                            </span>
-                          </>
-                        );
-                      }
-                    )}
-                </span>
-              </div>
+                    </div>
+                    <div className='right-header__line2'>
+                        <span>
+                            {
+                                dataHeader && dataHeader.length > 0 && dataHeader[1].attributes.menuheader.link.map((item,index) => {
+                                    return <span>
+                                        <span>
+                                        <span className={index !== 3 && 'span-left'}>
+                                            <span className='mid-header__icon' dangerouslySetInnerHTML={{__html:item.icon}}/>
+                                            <span key={item.id}>{item.label}</span>
+                                        </span>    
+                                        </span>
+                                    </span>
+                                })
+                            }
+                        </span>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-        <span className="between-line"></span>
-        <SubHeader data={dataHeader} />
-      </div>
+        <div style={{width:'100%', border:'1px solid #d4d4d4'}}></div>
+        <SubHeader data={dataHeader}></SubHeader>
     </>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
