53fafe77c20ade9bb106d9cdac6aa6f45c671c33
hiepvp <hiep23899@gmail.com>
2023-03-17 13:55:58 +0700

    up

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -12,9 +12,10 @@ export const cartSlice = createSlice({
   reducers: {
 
     addToCart: (state, { payload }) => {
-      // toast.success(`Đã thêm ${payload.name} vào giỏ hàng`);
+      toast.success(`Đã thêm ${payload.name} vào giỏ hàng`);
       const addItem = state.cartItem.find(item => item.name == payload.name)
       const cartItem = addItem ? state.cartItem.map(x => x.name == addItem.name ? { ...x, quantity: x.quantity + 1 } : x) : [...state.cartItem, { ...payload, quantity: Number(1) }]
+      console.log(cartItem);
       localStorage.setItem('cart', JSON.stringify(cartItem)); // Lưu giỏ hàng vào LocalStorage
       return { ...state, cartItem }
     },
