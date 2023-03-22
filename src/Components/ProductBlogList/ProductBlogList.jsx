2a5bac9b822abc00a55508acfdf7533ce8f16309
Manh Cuong <cuongmanh0910@gmail.com>
2023-03-22 20:59:15 +0700

    cuong update

--- src/Components/ProductBlogList/ProductBlogList.jsx
+++ src/Components/ProductBlogList/ProductBlogList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-
+import './ProductBlogList.css'
 export default function ProductBlogList() {
   const [blogList, setblogList] = useState([])
   useEffect(() => {
@@ -10,17 +10,44 @@ export default function ProductBlogList() {
       })
   }, [])
   return (
-    <div>
-      {blogList.map((item) => {
+    <div className='blog-list-container'>
+      <h2 className="blog-list__title">PC GEARVN - TOP LIST PC NỔI BẬT</h2>
+      <div className='row'>
+        <div className='col-10' style={{ borderRight: '1px solid silver', paddingRight: '20px' }}>
+          {blogList.map((item) => {
+            return <div className='row'>
+              <div className='col-4'>
+                <img src={`https://backoffice.nodemy.vn${item.attributes.image.data[0].attributes.url}`}
+                  alt="">
+                </img>
+              </div>
+              <div className='col-8 pt-5'>
+                <h2 className='fw-bold text-danger'>{item.attributes.name}</h2>
+                <p className='overflow-hidden'>{item.attributes.description}</p>
+                <p>CPU : {item.attributes.cpu}</p>
+                <p>RAM : {item.attributes.ram}</p>
+              </div>
+            </div>
 
-        return <div className='container' style={{ width: '60%', margin: '0 auto' }}>
-          <div className='row mt-3 mb-3' style={{border:'1px solid silver'}}>
-            <img src={`https://backoffice.nodemy.vn${item.attributes.image.data[0].attributes.url}`}
-              alt="" style={{ width: '50%', margin: '0 auto' }}></img>
-              <p className='text-center text-danger fw-bold pb-3'>{item.attributes.name}</p>
+          })}
+        </div>
+
+        <div className='col-2 blog-list__right'>
+          <div className='row'>
+            <img src='https://w.ladicdn.com/s500x900/5bf3dc7edc60303c34e4991f/11-side-web-230x679-20230301043030-u7pnn.png' alt=''></img>
+          </div>
+          <div className='row mt-3'>
+            <img src='https://w.ladicdn.com/s500x900/5bf3dc7edc60303c34e4991f/side-web-20230214022014-feh4d-20230313033643-ws0dz.png' alt=''></img>
+          </div>
+          <div className='row mt-3'>
+            <img src='https://w.ladicdn.com/s550x950/5bf3dc7edc60303c34e4991f/side-web-20230221090231-qrep_.png' alt=''></img>
+          </div>
+          <div className='row mt-3'>
+            <img src='https://w.ladicdn.com/s550x950/5bf3dc7edc60303c34e4991f/side-web-20230213133317-_tcfj.png' alt=''></img>
           </div>
         </div>
-      })}
+      </div>
+
     </div>
   )
 }
